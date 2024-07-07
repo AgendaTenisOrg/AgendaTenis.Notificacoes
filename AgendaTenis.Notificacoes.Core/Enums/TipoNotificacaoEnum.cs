363c62d07def52aa3a234945b09055bb855ed885
matheuscm (Matheus Campanini) <matheus.campanini@ploomes.com>
2024-07-07 15:44:56 -0300

    Criado o projeto de Notificações

---
+++ AgendaTenis.Notificacoes.Core/Enums/TipoNotificacaoEnum.cs
@@ -0,0 +1,9 @@
+﻿namespace AgendaTenis.Notificacoes.Core.Enums;
+
+public enum TipoNotificacaoEnum
+{
+    Erro = 0,
+    Aviso = 1,
+    Informacao = 2
+}
+
---
+++ AgendaTenis.Notificacoes.Core/Notificacao.cs
@@ -0,0 +1,9 @@
+﻿using AgendaTenis.Notificacoes.Core.Enums;
+
+namespace AgendaTenis.Notificacoes.Core;
+
+public class Notificacao
+{
+    public string Mensagem { get; set; }
+    public TipoNotificacaoEnum Tipo { get; set; }
+}
